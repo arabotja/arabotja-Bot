2e2e9a151c3b108116c486331f6c87cba38daa00
arabotja <arabotja@gmail.com>
2013-12-09 01:52:26 +0900

    comment added about 3 log files
    주석 추가

--- arabotja_turnKey.py
+++ arabotja_turnKey.py
@@ -4,5 +4,7 @@
 import os
 from arabotja import BabyBird
 
-arabotja = BabyBird('ilbe')
+arabotja = BabyBird('ilbe') 
 arabotja.wakeBird()
+
+# If you use 'crontab', you have to find the 3 log files in /home/yourName/ after arabotja_turnKey.py or just change the source
