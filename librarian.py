ca3eeea28c4a2b7bc1361c2f129c851afae8ab54
arabotja <arabotja@gmail.com>
2013-12-09 01:40:16 +0900

    added comment
    주석 추가

--- librarian.py
+++ librarian.py
@@ -9,15 +9,15 @@ class DumpTruck(Bibliomaniac):
 	def __init__(self, name):
 		Bibliomaniac.__init__(self, name)
 
-	def makeFilter(self, keyword, lastpage):
+	def makeFilter(self, keyword, lastpage): # When you turn key, recent article list become two filters' base
 		self.bindPicture(keyword, lastpage)
 	
-		l_f = open(self.name + '_lightFilter.bin', 'wb') # l_f for light_file
+		l_f = open(self.name + '_lightFilter.bin', 'wb') # l_f for light_file; Copying of last recent article list
 		pickle.dump(self.routes, l_f)
 		l_f.close()
 		del l_f
 
-		f_f = open(self.name + '_fineFilter.bin', 'wb') # f_f for fine_file
+		f_f = open(self.name + '_fineFilter.bin', 'wb') # f_f for fine_file; Incremental filter list from your turn key 
 		pickle.dump(self.routes, f_f)
 		f_f.close()
 		del f_f
@@ -30,7 +30,7 @@ class DumpTruck(Bibliomaniac):
 		r_f.close()
 		del r_f
 	
-	def judgeArticle(self):
+	def judgeArticle(self): # Filter core
 
 		r_f = open(self.name + '_recentTweet.bin', 'rb') 
 		recentTweet = pickle.load(r_f)
@@ -48,7 +48,7 @@ class DumpTruck(Bibliomaniac):
 		del f_f
 
 		i = 0
-		while i < 5:
+		while i < 5: # Old one? or New one?
 			
 			if recentTweet[i] in lightFilter:
 				print '-' * 50 + 'Old one...Already tweeted.'
@@ -62,7 +62,7 @@ class DumpTruck(Bibliomaniac):
 
 			i += 1
 
-		for e in self.basket:
+		for e in self.basket: # Update fine filter
 			self.fineFilter.insert(0, e)
 
 		f_f = open(self.name + '_fineFilter.bin', 'wb')
@@ -71,7 +71,7 @@ class DumpTruck(Bibliomaniac):
 		del f_f
 		print '-' * 50 + 'Updated _fineFilter.bin.'
 
-		l_f = open(self.name + '_lightFilter.bin', 'wb')
+		l_f = open(self.name + '_lightFilter.bin', 'wb') # Update light filter
 		pickle.dump(recentTweet, l_f)
 		l_f.close()
 		del l_f
